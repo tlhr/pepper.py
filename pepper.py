b781affbb500ef85ecd2597314aeaf5ca16648fb
brobostigon <tommyloehr@gmail.com>
2016-02-18 00:15:50 +0100

    removed rdkit dependency, fixed incorrect initial mass calculation

--- pepper.py
+++ pepper.py
@@ -3,17 +3,67 @@ from rdkit import Chem
 from rdkit.Chem.Descriptors import ExactMolWt
 
 
-masses = {'Boc':  101.126,
-          'tBu':  73.116,
-          'Tfa':  97.017,
-          'Pbf':  253.338,
-          'Trt':  243.331}
+orig_masses = {'Boc':  101.126,
+               'tBu':  73.116,
+               'Tfa':  97.017,
+               'Pbf':  253.338,
+               'Trt':  243.331,
+               'red':  -28.0140 + 2 * 1.008,
+               'Fmoc': 223.254}
+
+misc_masses = {'H2O':  18.0153,
+               'H':    1.008}
+
+aamw = {'I': 131.1736,
+        'L': 131.1736,
+        'K': 146.1882,
+        'M': 149.2124,
+        'F': 165.1900,
+        'T': 119.1197,
+        'W': 204.2262,
+        'V': 117.1469,
+        'R': 174.2017,
+        'H': 155.1552,
+        'A': 89.0935,
+        'N': 132.1184,
+        'D': 133.1032,
+        'C': 121.1590,
+        'E': 147.1299,
+        'Q': 146.1451,
+        'G': 75.0669,
+        'P': 115.1310,
+        'S': 105.0930,
+        'Y': 181.1894,
+        'a': 113.1160,
+        'b': 206.2050}
+
+masses = {k: v - misc_masses['H'] for k, v in orig_masses.items()}
 
 scs = {'Boc': ['K', 'H', 'W'],
        'tBu': ['Y', 'S', 'T', 'D', 'E'],
        'NH2': ['K', 'H', 'W', 'Q', 'N'],
        'Pbf': ['R'],
-       'Trt': ['K', 'H', 'W', 'Q', 'N']}
+       'Trt': ['K', 'H', 'W', 'Q', 'N'],
+       'red': ['b']}
+
+
+def pepweight(seq):
+    """
+    Calculate peptide mass based on MW table.
+
+    Parameters
+    ----------
+    seq : string
+        One letter code sequence string
+
+    Returns
+    -------
+    peptide_weight : float
+        Exact mol. weight of peptide
+
+    """
+    return (sum(aamw[aa] for aa in seq) -
+            (len(seq) - 1) * misc_masses['H2O'])
 
 
 def pepattr(seq):
@@ -39,7 +89,9 @@ def pepattr(seq):
             if aa in scs[sc]:
                 permut[sc] += 1
     # Output ranges instead of amounts
-    return {k: range(0, 1 + v) for k, v in permut.items()}
+    rperm = {k: range(0, 1 + v) for k, v in permut.items()}
+    rperm['Fmoc'] = [1]
+    return rperm
 
 
 def pepcut(seqlist, n=3):
@@ -88,10 +140,14 @@ def permute(seq, target_mass, eps=0.9, N=3):
         Number of amino acids to cut from the sequence
 
     """
-    mol = Chem.rdmolfiles.MolFromSequence(seq)
-    orig_mass = ExactMolWt(mol)
+    prev_mass = orig_mass = 0
     for p in pepcut([seq], N):
-        print(p)
+        prev_mass = orig_mass
+        # mol = Chem.rdmolfiles.MolFromSequence(p)
+        # orig_mass = ExactMolWt(mol)
+        orig_mass = pepweight(seq)
+        if prev_mass == orig_mass:
+            continue
         # Create all possible permutations from side chains and sequences
         for pp in product(*pepattr(p).values()):
             test_mass = orig_mass
@@ -104,8 +160,13 @@ def permute(seq, target_mass, eps=0.9, N=3):
 
 
 def main():
-    seq = 'KLKARIRRLFGGY'
-    permute(seq, 4029., 1.0)
+    seq = 'YGGbLRRIRAKaK'
+    # mol = Chem.rdmolfiles.MolFromSequence(seq)
+    # rdw = ExactMolWt(mol)
+    # w = pepweight(seq)
+    # print(rdw, w, abs(rdw - w))
+    permute(seq, 1762., 9.9, 3)
+    # AAKLKDRRRKKD +Boc, +Pbf
 
 
 if __name__ == '__main__':
